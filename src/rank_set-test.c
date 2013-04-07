ac0702df98c4f2590ab8cbd8ef7c67d6b2b278f3
Kevin <kevin.waugh@gmail.com>
2013-04-07 17:44:34 -0400

    added rank_set_valid test

--- src/rank_set-test.c
+++ src/rank_set-test.c
@@ -1,8 +1,14 @@
 #include "rank_set.h"
 #include "test.h"
 
-TEST(card_set_size) {
+TEST(rank_set_size) {
   expect(rank_set_size(EMPTY_CARD_SET) == 0);
   expect(rank_set_from_rank(0) == 1);
   expect(rank_set_from_rank_array(3, (card_t[]){0,1,2}) == 3);
+}
+
+TEST(rank_set_valid) {
+  for(rank_set_t i=0; i<(1u<<RANKS); ++i) {
+    expect(rank_set_valid(i));
+  }
 }
