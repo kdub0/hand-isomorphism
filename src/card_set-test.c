a8216d2323a16fca74c8a8bed503db6faaa53a48
Kevin <kevin.waugh@gmail.com>
2013-04-07 17:32:10 -0400

    added card_set_size test, failing

--- src/card_set-test.c
+++ src/card_set-test.c
@@ -1,4 +1,8 @@
 #include "card_set.h"
 #include "test.h"
 
-
+TEST(card_set_size) {
+  expect(card_set_size(EMPTY_CARD_SET) == 0);
+  expect(card_set_from_card(0) == 1);
+  expect(card_set_from_rank_array(3, (card_t[]){0,1,2}) == 3);
+}
